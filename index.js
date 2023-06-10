250ab126cbc49485f6c4441ba1efe5f76fcafad2
mim2041 <suraiyamim338@gmail.com>
2023-06-10 23:44:38 +0600

    instructors data fetched

--- index.js
+++ index.js
@@ -33,8 +33,11 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/instructors', async(req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    })
 
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
