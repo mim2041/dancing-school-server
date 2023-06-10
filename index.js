2438a461a1633c4f2898db870e08ba934e6fff8a
mim2041 <suraiyamim338@gmail.com>
2023-06-11 00:08:09 +0600

    user info saved to mongodb

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ async function run() {
       res.send(result);
     })
 
+    
+    // save user info
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      const query = { email: userInfo.email };
+      const user = await usersCollection.findOne(query);
+      if (!user) {
+        const result = await usersCollection.insertOne(userInfo);
+        res.send(result);
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
